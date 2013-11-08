6816e7181dfe8c44e9686472e587ca60687e49b6
pchiang <rukqoa@gmail.com>
2013-11-08 09:07:04 -0800

    first commit and probably last

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -1,32 +1,72 @@
 /* earthquakes.js
-    Script file for the INFO 343 Lab 7 Earthquake plotting page
+Script file for the INFO 343 Lab 7 Earthquake plotting page
 
-    SODA data source URL: https://soda.demo.socrata.com/resource/earthquakes.json
-    app token (pass as '$$app_token' query string param): Hwu90cjqyFghuAWQgannew7Oi
+SODA data source URL: https://soda.demo.socrata.com/resource/earthquakes.json
+app token (pass as "$$app_token" query string param): Hwu90cjqyFghuAWQgannew7Oi
 */
 
 //create a global variable namespace based on usgs.gov
-//this is how JavaScript developers keep global variables
-//separate from one another when mixing code from different
-//sources on the same page
 var gov = gov || {};
 gov.usgs = gov.usgs || {};
-
-//base data URL--additional filters may be appended (see optional steps)
-//the SODA api supports the cross-origin resource sharing HTTP header
-//so we should be able to request this URL from any domain via AJAX without
-//having to use the JSONP technique
-gov.usgs.quakesUrl = 'https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi';
-
-//current earthquake dataset (array of objects, each representing an earthquake)
-gov.usgs.quakes;
-
-//reference to our google map
-gov.usgs.quakesMap;
+gov.usgs.quakesUrl = "https://soda.demo.socrata.com/resource/earthquakes.json?$$app_token=Hwu90cjqyFghuAWQgannew7Oi";
+gov.usgs.quakes
+gov.usgs.quakesMap
 
 //AJAX Error event handler
 //just alerts the user of the error
-$(document).ajaxError(function(event, jqXHR, err){
-    alert('Problem obtaining data: ' + jqXHR.statusText);
+$(document).ajaxError(function(event, jqXHR, err) {
+    alert("Problem obtaining data: " + jqXHR.statusText);
 });
 
+$(function() {
+    getQuakes();
+});
+
+// Get earthquakes data
+function getQuakes() {
+    $.getJSON(gov.usgs.quakesUrl, function(quakes) {
+        gov.usgs.quakes = quakes;
+        $(".message").html("Displaying " + quakes.length + " earthquakes");
+
+        gov.usgs.quakesMap = new google.maps.Map($(".map-container")[0], {
+            center : new google.maps.LatLng(0, 0),
+            zoom : 2,
+            mapTypeId : google.maps.MapTypeId.TERRAIN,
+            streetViewControl : false
+        });
+
+        addQuakeMarkers(quakes, gov.usgs.quakesMap);
+    });
+
+    // Add loading message
+    $(".message").html("Loading... <img src=\"img/loading.gif\">");
+}
+
+//loop over the quakes array and add a marker for each quake
+function addQuakeMarkers(quakes, map) {
+    //loop over the quakes array and add a marker for each quake
+    var quake;
+
+    for (var i = 0; i < quakes.length; i++) {
+        quake = quakes[i];
+        quake.mapMarker = new google.maps.Marker({
+            map : map,
+            position : new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+        });
+
+        google.maps.event.addListener(quake.mapMarker, "click", function() {
+            // Automatically close
+            if (gov.usgs.iw) {
+                gov.usgs.iw.close();
+            }
+
+            //create an info window with the quake info
+            gov.usgs.iw = new google.maps.InfoWindow({
+                content : new Date(quake.datetime).toLocaleString() + ": magnitude " + quake.magnitude + " at depth of " + quake.depth + " meters"
+            });
+
+            //open the info window
+            gov.usgs.iw.open(map, this);
+        });
+    }
+}
